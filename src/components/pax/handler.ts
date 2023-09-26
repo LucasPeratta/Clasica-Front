4aca0f3d92d76e0074fd46fef57e47776ea13153
Lucas Peratta <lucasperatta@gmail.com>
2023-09-26 13:42:22 -0300

    Arreglando rutas

--- src/components/pax/handler.ts
+++ src/components/pax/handler.ts
@@ -1,8 +1,10 @@
 import { iPax } from "../model";
 
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
+
 export const getPax = async (): Promise<iPax[]> => {
   try {
-    const response = await fetch("http://localhost:3001/api/pax");
+    const response = await fetch(`${apiUrl}/pax`);
     const data = await response.json();
     return data.paxs;
   } catch (error) {
@@ -13,7 +15,7 @@ export const getPax = async (): Promise<iPax[]> => {
 
 export const deletePax = async (id: string): Promise<void> => {
   try {
-    await fetch(`http://localhost:3001/api/pax/${id}`, {
+    await fetch(`${apiUrl}/pax/${id}`, {
       method: "DELETE",
     });
   } catch (error) {
@@ -23,7 +25,7 @@ export const deletePax = async (id: string): Promise<void> => {
 
 export const getPaxById = async (id: string) => {
   try {
-    const response = await fetch(`http://localhost:3001/api/pax/${id}`);
+    const response = await fetch(`${apiUrl}/pax/${id}`);
     const data = await response.json();
     return data.data;
   } catch (error) {
@@ -33,14 +35,13 @@ export const getPaxById = async (id: string) => {
 
 export const updatePax = async (id: string, formData: iPax) => {
   try {
-    const response = await fetch(`http://localhost:3001/api/pax/update/${id}`, {
+    const response = await fetch(`${apiUrl}/pax/update/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
     });
-    // const data = await response.json();
     return response;
   } catch (error) {
     console.error("Error de red:", error);
@@ -50,7 +51,7 @@ export const updatePax = async (id: string, formData: iPax) => {
 
 export const createPax = async (formData: iPax): Promise<Response> => {
   try {
-    const response = await fetch("http://localhost:3001/api/pax/create", {
+    const response = await fetch(`${apiUrl}/pax/create`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
