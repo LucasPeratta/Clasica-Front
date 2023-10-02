6dea493829e62d7b3307854641f2553dd617ff49
Lucas Peratta <lucasperatta@gmail.com>
2023-10-02 17:25:27 -0300

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-// vite.config.js
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default {
-  server: {
-    port: 3000, // Cambia el puerto a tu valor deseado
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.SOME_KEY": JSON.stringify(env.SOME_KEY),
+    },
+    plugins: [react()],
+    server: {
+      port: 3000, // Cambia el puerto a tu valor deseado
+    },
+  };
+});
