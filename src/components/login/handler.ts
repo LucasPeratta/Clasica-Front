67981b0d48b412283338ce3e34ad36ba19d137fb
Lucas Peratta <lucasperatta@gmail.com>
2023-10-02 13:23:14 -0300

    change login's handler

--- src/components/login/handler.ts
+++ src/components/login/handler.ts
@@ -1,6 +1,8 @@
+const apiUrl = process.env.REACT_APP_API_URL;
+
 export const loginHandler = async (email: string, password: string) => {
   try {
-    const response = await fetch("http://localhost:3001/api/login", {
+    const response = await fetch(`${apiUrl}/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
